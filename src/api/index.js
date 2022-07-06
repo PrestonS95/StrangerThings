3cc601fa4c8e28dfbd966147f330ab916fdab69a
PrestonS95 <prestonsullins95@gmail.com>
2022-07-06 10:50:12 -0500

    first updates to files

---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const api = `https://strangers-things.herokuapp.com/api/2206-FTB-ET-WEB-FT-A/`
+
+export default api
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 import { Router, BrowserRouter, Routes, Route,} from "react-router-dom";
 import { App } from './components'
+import { api } from './api'
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
