2cd149008be13741933a19d578a08cfc142419e6
PrestonS95 <prestonsullins95@gmail.com>
2022-07-06 10:11:29 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+import { Router, BrowserRouter, Routes, Route,} from "react-router-dom";
+import { App } from './components'
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+<BrowserRouter>
+    <App/>
+</BrowserRouter>
+);
